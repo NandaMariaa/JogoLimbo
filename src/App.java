016f60db3f1d482cdf6b7476b33599455780693e
MariaNand4 <Nandamariav1@gmail.com>
2024-04-30 14:12:36 -0300

    oi

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("olaaaaaaaaaaaaa");
+        System.out.println("o capitalismo falhou, falha e falhara");
     }
 }
