a97268fc2c6e7c983f939cfed25d241809a4d5fc
MariaNand4 <168575583+NandaMariaa@users.noreply.github.com>
2024-04-30 16:01:09 -0300

    oi

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.concurrent.TimeUnit;
+
 public class App {
+    static int temp_dialog = 80;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("o capitalismo falhou, falha e falhara");
+        Digita("\nvenha até mim", TimeUnit.MILLISECONDS, temp_dialog);
+        Digita("\nvenha até mim", TimeUnit.MILLISECONDS, temp_dialog);
+        Digita("\nvenha até mim", TimeUnit.MILLISECONDS, temp_dialog);
+        Digita("\nvenha até mim", TimeUnit.MILLISECONDS, temp_dialog);
+        Digita("\nvenha até mim", TimeUnit.MILLISECONDS, temp_dialog);
+        Digita("\nvenha até mim", TimeUnit.MILLISECONDS, temp_dialog);
+        Digita("\nvenha até mim", TimeUnit.MILLISECONDS, temp_dialog);
+    }
+
+    public static void Digita(String mensagem, TimeUnit unit, long tempo_mensagem) throws InterruptedException {
+        for (char caractere : mensagem.toCharArray()) {
+            System.out.print(caractere);
+            unit.sleep(tempo_mensagem);
+        }
     }
 }
